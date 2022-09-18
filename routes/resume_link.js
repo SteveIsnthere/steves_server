fb5dcd22b0f74bb565c6879c32167a8ff517ed18
stevewang <privacyalwaysfirst@proton.me>
2022-09-17 22:08:12 -0700

    fix

--- routes/resume_link.js
+++ routes/resume_link.js
@@ -5,7 +5,8 @@ let redirect_uri = "https://www.google.com";
 
 // define the home page route
 router.get('/', logger, (req, res) => {
-    res.redirect(redirect_uri)
+    // res.redirect(redirect_uri)
+    res.send("if you're seeing this, my server's dead again. Please email me at privacyalwaysfirst@proton.me")
 })
 
 module.exports = router
